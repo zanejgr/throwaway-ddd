a2d12e7fb6a6fd8aed2f9868c3dcc8bb1ec2b465
zane gray <zanejgr@gmail.com>
2022-01-05 13:44:26 -0600

    asdf

---
+++ Persistence/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+namespace Persistence;
+
+public class Root{
+    public int RootId {get;set;}
+    public string Name{get;set;}
+    public bool IsOkay { get;set;}
+    public List<UniqueProperty> UniqueProperties { get; set; } = new();
+    public List<SharedObject> SharedObjects { get; set; } = new();
+}
+public class UniqueProperty{
+    public string Id { get; set; }
+}
+public class SharedObject{
+    public string Id { get; set; }
+    public List<Root> Roots {get; set;} = new();
+}
+public class ApplicationContext : DbContext{
+    public DbSet<Root> Roots { get; set; } = default!;
+    public DbSet<UniqueProperty> UniqueProperties { get;set;} = default!;
+    public DbSet<SharedObject> SharedObjects { get;set;} = default!;
+    protected override void OnConfiguring(DbContextOptionsBuilder options){
+        options.UseSqlite("Data Source=persistence.db");
+    }
+}
